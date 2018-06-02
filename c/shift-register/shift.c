e865a305da23fb32e95c4b194b7b8dc752947a27
adidinchuk <adidinchuk@gmail.com>
2018-06-02 12:54:03 -0400

    cleaned up shift.c

--- c/shift-register/shift.c
+++ c/shift-register/shift.c
@@ -1,19 +1,19 @@
 #include <wiringPi.h>
 
-void pulse(int pin, double t){
-  digitalWrite (pin, HIGH) ;
-  delay (t) ;
-  digitalWrite (pin, LOW);
-}
+void pulse (int pin, double t);
 
-int main (void)
-{
-  int DS = 4;
-  int  CLK = 5;
-  int SHCP = 6;
-  double pulseTime = 0.01 ;
-  wiringPiSetup () ;
-  int data[10][7] = {
+// shift register data signal
+int DS = 4;
+// artificial clock signal for shift register
+int CLK = 5;
+// data out signal for the shift register
+int SHCP = 6;
+
+// pulse time for shift register
+double pulseTime = 5;
+
+// 7 segment display encoding 0-9
+int digits[10][7] = {
     {1, 1, 1, 1, 1, 1, 0},
     {0, 1, 1, 0, 0, 0, 0},
     {1, 1, 0, 1, 1, 0, 1},
@@ -25,17 +25,17 @@ int main (void)
     {1, 1, 1, 1, 1, 1, 1},
     {1, 1, 1, 1, 0, 1, 1}
   };
-  printf("%d, World\n", sizeof(data)/sizeof(data[0])); 
+
+int main (void){
+  wiringPiSetup () ;
+  printf("%d, World\n", sizeof(digits)/sizeof(digits[0])); 
   pinMode (DS, OUTPUT) ;
   pinMode (CLK, OUTPUT) ;
   pinMode (SHCP, OUTPUT) ;
-  for (;;)
-  {
-    pulse(CLK, 50) ;
-   for (int i = 0; i < sizeof(data)/sizeof(data[0]); i++)
-   {
-     for (int x = 0; x < sizeof(data[i])/sizeof(data[i][0]); x++)
-     {
+  for (;;)  {
+   pulse(CLK, 50) ;
+   for (int i = 0; i < sizeof(digits)/sizeof(digits[0]); i++)   {
+     for (int x = 0; x < sizeof(digits[i])/sizeof(digits[i][0]); x++)     {
        digitalWrite (DS, data[i][x]);
        pulse(CLK, 50);
      }
@@ -44,4 +44,13 @@ int main (void)
    }
   }
   return 0 ;
+}
+
+// pulse function to pulse a pin
+// pin - GPIO pin to pulse
+// t - pulse delay between high and low
+void pulse(int pin, double t){
+  digitalWrite (pin, HIGH) ;
+  delay (t) ;
+  digitalWrite (pin, LOW);
 }
