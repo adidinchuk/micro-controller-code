e671e974bc4605afc81eefb041797669984bf363
AD <adidinchuk@gmail.com>
2018-07-15 16:26:19 +0000

    added pythong nRF24 files

---
+++ python/nrf24/SendPi2.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Example program to send packets to the radio link
+#
+
+import sys
+import Adafruit_DHT
+import RPi.GPIO as GPIO
+from lib_nrf24 import NRF24
+import time
+import spidev
+
+GPIO.setmode(GPIO.BCM)
+
+pipes = [[0xe7, 0xe7, 0xe7, 0xe7, 0xe7], [0xc2, 0xc2, 0xc2, 0xc2, 0xc2]]
+
+radio = NRF24(GPIO, spidev.SpiDev())
+
+transmit_pin = 22
+dht11_data_pin = 18
+
+GPIO.setup(dht11_data_pin, GPIO.OUT)
+GPIO.output(dht11_data_pin, GPIO.HIGH)
+
+radio.begin(0, transmit_pin) #Set spi-ce pin10, and rf24-CE pin 8
+time.sleep(1)
+radio.setRetries(15,15)
+radio.setPayloadSize(32)
+radio.setChannel(0x60)
+
+radio.setDataRate(NRF24.BR_2MBPS)
+radio.setPALevel(NRF24.PA_MIN)
+radio.setAutoAck(True)
+radio.enableDynamicPayloads()
+radio.enableAckPayload()
+
+
+radio.openWritingPipe(pipes[1])
+radio.openReadingPipe(1, pipes[0])
+radio.printDetails()
+
+
+c=1
+while True:
+    buf = ['H', 'E', 'L', 'O',c]
+    c = (c + 1) & 255
+    # send a packet to receiver
+    humidity, temperature = Adafruit_DHT.read_retry(11, dht11_data_pin)
+    buf = 'Temp ' + str(temperature) + ', Humi ' + str(humidity) 	
+    radio.write(buf)
+    print ("Sent:"),
+    print (buf)
+    # did it return with a payload?
+    if radio.isAckPayloadAvailable():
+        pl_buffer=[]
+        radio.read(pl_buffer, radio.getDynamicPayloadSize())
+        print ("Received back:"),
+        print (pl_buffer)
+    else:
+        print ("Received: Ack only, no payload")
+    time.sleep(10)
