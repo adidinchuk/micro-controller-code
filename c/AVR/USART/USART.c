326ab54a45108b8138f2835f4357fb4dd546aef6
adidinchuk <adidinchuk@gmail.com>
2018-07-30 22:49:59 -0400

    Added dp to 7seg

--- c/AVR/USART/USART.c
+++ c/AVR/USART/USART.c
@@ -21,16 +21,16 @@
 #include <util/setbaud.h>
 
 void initUSART(void) {                                /* requires BAUD */
-  UBRR0H = UBRRH_VALUE;                        /* defined in setbaud.h */
-  UBRR0L = UBRRL_VALUE;
+  UBRRH = UBRRH_VALUE;                        /* defined in setbaud.h */
+  UBRRL = UBRRL_VALUE;
 #if USE_2X
-  UCSR0A |= (1 << U2X0);
+  UCSRA |= (1 << U2X0);
 #else
-  UCSR0A &= ~(1 << U2X0);
+  UCSRA &= ~(1 << U2X0);
 #endif
                                   /* Enable USART transmitter/receiver */
-  UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-  UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);   /* 8 data bits, 1 stop bit */
+  UCSRB = (1 << TXEN0) | (1 << RXEN0);
+  UCSRC = (1 << UCSZ01) | (1 << UCSZ00);   /* 8 data bits, 1 stop bit */
 }
 
 
